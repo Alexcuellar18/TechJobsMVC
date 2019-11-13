782b3e129bce85fb7fc39737df94483f82fb13c8
Alejandro C <alejandrocuellar18@gmail.com>
2019-11-12 23:44:18 -0500

    Finished project. Changes to Search/List controllers and their views.

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -6,15 +6,43 @@ namespace TechJobs.Controllers
 {
     public class SearchController : Controller
     {
+        private static List<Dictionary<string, string>> searchResults = new List<Dictionary<string, string>>();
+
         public IActionResult Index()
         {
             ViewBag.columns = ListController.columnChoices;
             ViewBag.title = "Search";
+            ViewBag.jobs = searchResults;
             return View();
         }
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        [HttpPost]
+        [Route("/Search")]
+        public IActionResult Results(string searchType,string searchTerm)
+        {
+            ViewBag.columns = ListController.columnChoices;
+            searchType = searchType.ToLower();
+            searchTerm = searchTerm.ToLower();
 
+
+            
+            
+            if (searchType == "all")
+            {
+                searchResults = JobData.FindByValue(searchTerm);
+              
+            }
+            else
+            {
+                searchResults = JobData.FindByColumnAndValue(searchType, searchTerm);
+                
+            }
+            ViewBag.jobs = searchResults;
+            //TempData["jobs"] = searchResults;
+
+            return View("Index");
+        }
     }
 }
